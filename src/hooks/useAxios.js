0d9e352065338f58c3042b8491d5e071aa3c7af7
Shahabr99 <sean.rohani9@gmail.com>
2023-10-04 20:41:40 -0700

    part2 almost done

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,7 +1,28 @@
 import {useState, useEffect} from 'react';
+import axios from 'axios';
+import {v4 as uuid} from 'uuid';
 
-const useAxios = () => {
+const useAxios = (url) => {
 
+  const [response, setResponse] = useState(null)
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    async function getData() {
+        const res = await axios.get(url)
+        setResponse(res)
+      }
+      getData()
+  }, [url])
+
+  const addCards = () => {
+    setCards(data => [
+      ...data, 
+      {...response.data, id: uuid()}
+    ])
+  }
+
+  return [cards, addCards]
 }
 
 
